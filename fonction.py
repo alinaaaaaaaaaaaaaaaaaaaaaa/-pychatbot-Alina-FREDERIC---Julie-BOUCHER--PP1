d31ecdee14e3a74e16f916d008ecf41c45af7916
alinaaaaaaaaaaaaaaaaaaaaaa <151009238+alinaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-12-01 19:51:10 +0100

    version 1

--- fonction.py
+++ fonction.py
@@ -113,13 +113,113 @@ def calculate_idf(corpus_directory):
 
     return idf_scores
 
-# Exemple d'utilisation :
-corpus_directory = "./cleaned"  # Remplacez par votre répertoire de corpus
-idf_scores = calculate_idf(corpus_directory)
 
-# Affichez les scores IDF pour chaque mot
-for word, idf_score in idf_scores.items():
-    print(f"Mot : {word}, Score IDF : {idf_score}")
+##julie
+def calculate_tf_idf(corpus_directory):
+    document_frequency = Counter()
+    total_documents = 0
+    tf_scores = {}
+
+    for filename in os.listdir(corpus_directory):
+        if filename.endswith(".txt"):
+            total_documents += 1
+
+            file_path = os.path.join(corpus_directory, filename)
+            unique_words_in_document = set()
+
+            with open(file_path, 'r', encoding="utf-8") as file:
+                for line in file:
+                    cleaned_line = clean_line(line.lower())
+                    words_in_line = cleaned_line.split()
+                    unique_words_in_document.update(words_in_line)
+
+            # Assurez-vous de déclarer unique_words_in_document ici
+            tf_scores[filename] = {word: words_in_line.count(word) / len(words_in_line) if len(words_in_line) > 0 else 0 for word in unique_words_in_document}
+            document_frequency.update(unique_words_in_document)
+
+    idf_scores = {word: math.log(1 + (total_documents / (document_frequency[word]))) for word in document_frequency}
+    tf_idf_scores = {document: {word: tf_scores[document][word] * idf_scores[word] for word in tf_scores[document]} for document in tf_scores}
+
+    return tf_idf_scores
+
+
+##############
+##############
+##############
+##############
+
+
+def clean_line(line):
+    cleaned_line = line.replace("'", " ")
+    cleaned_line = cleaned_line.translate(str.maketrans("", "", string.punctuation))
+    return cleaned_line
+
+def calculate_idf(corpus_directory):
+    document_frequency = {}
+
+    total_documents = 0
+    for filename in os.listdir(corpus_directory):
+        if filename.endswith(".txt"):
+            total_documents += 1
+
+            file_path = os.path.join(corpus_directory, filename)
+            unique_words_in_document = set()
+
+            with open(file_path, 'r', encoding="utf-8") as file:
+                for line in file:
+                    cleaned_line = clean_line(line.lower())
+                    words_in_line = cleaned_line.split()
+                    unique_words_in_document.update(words_in_line)
+
+            for word in unique_words_in_document:
+                document_frequency[word] = document_frequency.get(word, 0) + 1
+
+    idf_scores = {word: math.log(total_documents / (1 + document_frequency[word])) for word in document_frequency}
+    return idf_scores
+
+#Matrice
+
+def generate_tfidf_matrix(corpus_directory):
+    idf_scores = calculate_idf(corpus_directory)
+    unique_words = sorted(idf_scores.keys())
+
+    tfidf_matrix = []
+
+    for filename in os.listdir(corpus_directory):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(corpus_directory, filename)
+
+            with open(file_path, 'r', encoding="utf-8") as file:
+                cleaned_lines = [clean_line(line.lower()) for line in file]
+                words_in_document = ' '.join(cleaned_lines).split()
+
+                tfidf_row = [idf_scores[word] * words_in_document.count(word) / len(words_in_document) for word in unique_words]
+                tfidf_matrix.append(tfidf_row)
+
+    return tfidf_matrix
+
+
+def transpose_matrix(matrix):
+    # Calculer le nombre de lignes et de colonnes de la matrice
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+
+    # Créer une matrice transposée remplie de zéros
+    transposed_matrix = [[0] * num_rows for _ in range(num_cols)]
+
+    # Remplir la matrice transposée
+    for i in range(num_rows):
+        for j in range(num_cols):
+            transposed_matrix[j][i] = matrix[i][j]
+
+    return transposed_matrix
+
+# Exemple d'utilisation :
+
+####
+def print_tfidf_matrix(matrix):
+    for row in matrix:
+        print(row)
 
 
 
--- main.py
+++ main.py
@@ -31,7 +31,7 @@ cleaned_directory = "./cleaned"
 # Obtenir la liste des noms de fichiers dans le répertoire nettoyé
 files_names = list_of_files(cleaned_directory, "txt")
 
-#TF
+#Occ
 
 cleaned_directory = "./cleaned"
 # Parcourir chaque fichier et imprimer les occurrences de chaque mot
@@ -41,7 +41,57 @@ for filename in files_names:
 
     print(f"Occurrences dans {filename}: {word_occurrences}")
 
+# IDF
 
+# Exemple d'utilisation :
+corpus_directory = "./cleaned"  # Remplacez par votre répertoire de corpus
+idf_scores = calculate_idf(corpus_directory)
+
+# Affichez les scores IDF pour chaque mot
+for word, idf_score in idf_scores.items():
+    print(f"Mot : {word}, Score IDF : {idf_score}")
+
+print("#############")
+
+##TF IDF
+corpus_directory = "./cleaned"
+tf_idf_scores = calculate_tf_idf(corpus_directory)
+
+for document, scores in tf_idf_scores.items():
+    print(f"Document : {document}")
+    for word, tf_idf_score in scores.items():
+        print(f"  Mot : {word}, Score TF-IDF : {tf_idf_score}")
+
+
+print("#############")
+
+#Matrice
+corpus_directory = "./cleaned"  # Remplacez par votre répertoire de corpus
+tfidf_matrix = generate_tfidf_matrix(corpus_directory)
+
+# Affichez la forme de la matrice TF-IDF
+print("la matrice TF-IDF avant transposition :", len(tfidf_matrix), "x", len(tfidf_matrix[0]))
+
+# Transposer la matrice TF-IDF
+transposed_tfidf_matrix = transpose_matrix(tfidf_matrix)
+
+# Affichez la forme de la matrice TF-IDF après transposition
+print("la matrice TF-IDF après transposition :", len(transposed_tfidf_matrix), "x", len(transposed_tfidf_matrix[0]))
+
+# Exemple d'utilisation :
+corpus_directory = "./cleaned"  # Remplacez par votre répertoire de corpus
+tfidf_matrix = generate_tfidf_matrix(corpus_directory)
+
+# Affichez la matrice TF-IDF
+print("#############################################################################Matrice TF-IDF avant transposition :")
+print_tfidf_matrix(tfidf_matrix)
+
+# Transposer la matrice TF-IDF si nécessaire
+transposed_tfidf_matrix = transpose_matrix(tfidf_matrix)
+
+# Affichez la matrice TF-IDF après transposition
+print("#############################################################################Matrice TF-IDF après transposition :")
+print_tfidf_matrix(transposed_tfidf_matrix)
 
 
 
